b7a0495d588a9c1eda8332df1c9a7ec13b5583b1
Siddhant Srivastava <122685718+sidd1092@users.noreply.github.com>
2024-08-07 05:13:33 +0530

    Time: 8 ms (84.73%), Space: 46.4 MB (68.08%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,37 +1,27 @@
 class Solution {
     public int[][] merge(int[][] arr) {
-        ArrayList<ArrayList<Integer>> LS = new ArrayList<>();
+        ArrayList<int[]> LS = new ArrayList<>();
         Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
-        // System.out.println(Arrays.toString(arr));
         int start = 0,end = 0,len=arr.length;
         for(int i = 0;i<len;i++)
         {
             start = arr[i][0];
             int max = arr[i][1];
-            // if()
             while(i<len-1 && max>=arr[i+1][0]){
                 i++;
                 max = Math.max(max,arr[i][1]);
             }
-            if(i>0)
-                end = Math.max(max,arr[i-1][1]);
-            else
-                end = arr[i][1];
-            ArrayList<Integer> ls = new ArrayList<>();
-            ls.add(start);
-            ls.add(end);
-            LS.add(ls);
+            end = max;
+            LS.add(new int[]{start,end});
         }
-        return convertToArray(LS);
-    }
-    public static int[][] convertToArray(ArrayList<ArrayList<Integer>> arrayList) {
-        int numRows = arrayList.size();
-        int[][] result = new int[numRows][2];
-        for (int i = 0; i < numRows; i++){
-            ArrayList<Integer> rowList = arrayList.get(i);
-            for (int j = 0; j < 2; j++)
-                result[i][j] = rowList.get(j);
+        len = LS.size();
+        int[][] res = new int[len][2];
+        for (int i = 0; i < len; i++) {
+            int[] nums = LS.get(i);
+            for (int j = 0; j < 2; j++) {
+                res[i][j] = nums[j];
+            }
         }
-        return result;
+        return res;
     }
 }
