10e9a179c51b559813768891536831c5e84b56d8
Siddhant Srivastava <122685718+sidd1092@users.noreply.github.com>
2024-09-02 19:43:47 +0530

    Time: 2 ms (62.48%), Space: 42.3 MB (86.15%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.java
+++ 3447-clear-digits/3447-clear-digits.java
@@ -1,19 +1,12 @@
 class Solution {
     public String clearDigits(String s) {
-        Stack<Character> stk = new Stack<>();int pop=0;
+        Stack<Character> stk = new Stack<>();
         for(char ch : s.toCharArray())
         {
             if(ch>='0' && ch<='9' && !stk.isEmpty())
                 stk.pop();
-            else if(ch>='0' && ch<='9' && stk.isEmpty())
-                pop++;
             else
             stk.push(ch);
-            while(!stk.empty() && pop>0)
-            {
-                stk.pop();
-                pop--;
-            } 
         }
         StringBuilder sb = new StringBuilder();
         for(char ch : stk)
