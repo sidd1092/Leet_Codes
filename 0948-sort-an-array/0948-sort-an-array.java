6ef28bf22ff17bd22668ee9431bcda28d8e238e2
Siddhant Srivastava <122685718+sidd1092@users.noreply.github.com>
2024-09-03 14:26:30 +0530

    Time: 26 ms (54.32%), Space: 56 MB (54.54%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,10 +1,9 @@
 class Solution {
-    public int[] sortArray(int[] arr) {
-         sort(arr, 0, arr.length - 1);
-         return arr;
-
+    public int[] sortArray(int[] nums) {
+        sort(nums, 0, nums.length-1);
+        return nums;
     }
-    static void sort(int arr[], int l, int r)
+    public static void sort(int arr[], int l, int r)
     {
         if (l < r) {
             int m = l + (r - l) / 2;
@@ -13,27 +12,29 @@ class Solution {
             merge(arr, l, m, r);
         }
     }
-    static void merge(int arr[], int l, int m, int r)
+    public static void merge(int arr[], int l, int m, int r)
     {
-        int n1 = m - l + 1;
-        int n2 = r - m;
+        int n1 = m-l+1;
+        int n2 = r-m;
         int L[] = new int[n1];
         int R[] = new int[n2];
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
+        for(int i = 0;i<n1;i++)
+            L[i] = arr[l+i];
+        for(int j = 0;j<n2;j++)
+            R[j] = arr[m+1+j];
         int i = 0, j = 0;
         int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j])
+        while(i < n1 && j < n2)
+        {
+            if(L[i] <= R[j])
                 arr[k++] = L[i++];
             else
                 arr[k++] = R[j++];
         }
-        while (i < n1) 
+        while(i < n1)
             arr[k++] = L[i++];
-        while (j < n2)
+        while(j < n2)
             arr[k++] = R[j++];
+        
     }
 }
