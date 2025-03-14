031050082a3f4b008a455820ca3d897151e47ce7
Siddhant Srivastava <122685718+sidd1092@users.noreply.github.com>
2025-03-14 22:04:44 +0530

    Time: 0 ms (100%), Space: 41 MB (76.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,8 @@
 class Solution {
     public int rob(int[] arr) {
         int n = arr.length;
-        if (n==1)
-        return arr[0];
-        if(n==2)
-            return Math.max(arr[0],arr[1]);
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        // return Math.max(func(dp,arr,n-1),func(dp,arr,n-2));
         return func(dp,arr,n-1);
     }
     static int func(int[]dp , int[]arr, int n ){
@@ -17,12 +12,8 @@ class Solution {
             return arr[0];
         if(dp[n]!=-1)
             return dp[n];
-        int pick = Integer.MIN_VALUE;
-        // if(n-2>=0)
-        pick = func(dp,arr,n-2) + arr[n];
-        int not_pick = pick;
-        if(n-1>=0)
-            not_pick = func(dp,arr,n-1) + 0;
+        int pick = func(dp,arr,n-2) + arr[n];
+        int not_pick = func(dp,arr,n-1) + 0;
         return dp[n] = Math.max(not_pick,pick);
     }
 }
