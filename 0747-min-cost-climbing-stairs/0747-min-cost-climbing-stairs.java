eafca083da596f82817f3b7ec6366cd5c49d6784
Siddhant Srivastava <122685718+sidd1092@users.noreply.github.com>
2025-03-14 19:36:31 +0530

    Time: 1 ms (38.64%), Space: 43.3 MB (48.69%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,19 +1,22 @@
 class Solution {
     public int minCostClimbingStairs(int[] arr) {
-        int n = arr.length;
+       int n = arr.length;
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return Math.min(func(arr, dp, n - 1), func(arr, dp, n - 2));
+        return Math.min(func(dp,arr,n-1),func(dp,arr,n-2));
     }
-    static int func(int[]arr, int[]dp, int n ){
+    static int func(int[]dp , int[]arr, int n ){
         if(n<=1)
             return arr[n];
         if(dp[n]!=-1)
             return dp[n];
-        int left = arr[n]+ func(arr,dp,n-1) ;
-        int right = left+1;
+        int left = func(dp,arr,n-1) + arr[n];
+        int right = left-1;
         if(n-2>=0)
-        right =  arr[n]+func(arr,dp,n-2) ;
-        return dp[n] = Math.min(right,left);
+        {
+            right = func(dp,arr,n-2) + arr[n];
+        }
+        return dp[n] = Math.min(left,right);
+    // return 20;
     }
 }
