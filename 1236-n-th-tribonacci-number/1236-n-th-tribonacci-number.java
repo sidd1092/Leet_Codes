bd072e2d347d2ea06a0f4eb3d81ea1747dd5d7b4
Siddhant Srivastava <122685718+sidd1092@users.noreply.github.com>
2025-03-14 16:58:02 +0530

    Time: 0 ms (100%), Space: 40.7 MB (27.35%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,21 +1,18 @@
 class Solution {
     public int tribonacci(int n) {
-        int []dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return f(n,dp);
+    int[] dp = new int[n+1];
+    Arrays.fill(dp,0);
+    return func(n,dp);
     }
-    public static int f(int index,int[] dp){
-        if(index==0){
+    private int func(int n ,int[] dp)
+    {
+        if(n==0)
             return 0;
-        }
-        if(index==1) return 1;
-        if(index==2) return 1;
-        if(dp[index]!=-1){
-            return dp[index];
-        }
-        int one = f(index-1,dp);
-        int two=f(index-2,dp);
-        int three=f(index-3,dp);
-        return dp[index]=one+two+three;
+        if (n<3)
+            return 1;
+        if(dp[n]!=0)
+            return dp[n];
+        dp[n] = func(n-1,dp)+func(n-2,dp)+func(n-3,dp);
+        return dp[n];
     }
 }
